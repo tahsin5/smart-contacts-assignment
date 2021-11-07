1b0f1da7d1734602925cdf0d69a24815215cc19a
100406205 <78794600+100406205@users.noreply.github.com>
2021-11-07 13:08:07 +0100

    Update exercise_1.sol

--- carlos/exercise_1.sol
+++ carlos/exercise_1.sol
@@ -3,21 +3,29 @@
 pragma solidity ^0.8.0;
 
 
-struct Place {
-    uint8 Number;
-    uint8 Row;
-}
-
-struct Seat {
+contract TicketBookingSystem {
+    // Try to implement it with an struct
+    /*struct Place{
+        uint8 Number;
+        uint8 Row;
+    }*/
+    uint8 Price;
     string Title;
     string Date;
-    uint8 Price;
-    mapping(uint => Place) place;
     string Link;
-}
-
-contract TicketBookingSystem {
-    {
-        
+    uint8 Number;
+    uint8 Row;
+    function initialize(uint8 pri, string memory tit, string memory dat, uint8 num, uint8 ro) public {
+        Price = pri;
+        Title = tit;
+        Date = dat;
+        Number = num;
+        Row = ro;
+        Link = "https://seatplan.com/";
     }
+    
+    function check_initialization() public view returns(uint8, string memory, string memory, string memory, uint8, uint8){
+        return (Price, Title, Date, Link, Number, Row);
+    }
+    
 }
