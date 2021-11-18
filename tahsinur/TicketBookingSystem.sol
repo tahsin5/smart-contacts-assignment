efa78596624736d2553cba3e9b9a764bc16bea14
Tahsinur Rahman <tahsinurrahman5@gmail.com>
2021-11-18 23:18:13 +0100

    TradeTicket implementation - throws error
    Error: transfer caller not owner nor approved

--- tahsinur/TicketBookingSystem.sol
+++ tahsinur/TicketBookingSystem.sol
@@ -52,6 +52,30 @@ contract TicketBookingSystem is ERC721URIStorage {
         uint8 row
     );
     
+     event TicketCancelled (
+        uint seatId,
+        address owner,
+        uint256 price,
+        string title,
+        string date,
+        string link,
+        uint8 number,
+        uint8 row,
+        string cancellation
+    );
+    
+    event TicketTransfer (
+        uint seatId,
+        address transferredFrom,
+        address transferredTo,
+        uint256 price,
+        string title,
+        string date,
+        string link,
+        uint8 number,
+        uint8 row
+    );
+    
 
     constructor() ERC721("Ticket Booking System", "TBS") {
         theatre = payable(msg.sender);
@@ -176,13 +200,95 @@ contract TicketBookingSystem is ERC721URIStorage {
     }
     
     
+    event Log(address indexed sender, string message);
+    event intLog(address indexed sender, uint256 message);
+    event seatLog(address indexed sender, Seat message);
     
     
     function verify(uint256 tokenID) public payable{
-        address buyer = address(this);
+        // address buyer = address(this);
         // require(buyer == idToSeat[tokenID].owner, "Not allowed to view")
         emit TicketSold(tokenID, idToSeat[tokenID].nftContract, idToSeat[tokenID].owner, idToSeat[tokenID].price, idToSeat[tokenID].title, idToSeat[tokenID].date, idToSeat[tokenID].link, idToSeat[tokenID].number, idToSeat[tokenID].row);
     }
+    
+    function refund(string memory title,
+        string memory date,
+        uint8 number,
+        uint8 row
+        ) public payable{
+        //To refund we just need to transfer the token to the theatre again
+        uint256 itemId = findSeat(title, date, number, row);
+        require(itemId != 0, "Seat not existing");
+        require(theatre != idToSeat[itemId].owner, "Seat not sold");
+        idToSeat[itemId].owner = theatre;
+        
+        emit TicketCancelled(itemId, idToSeat[itemId].owner, idToSeat[itemId].price, idToSeat[itemId].title, idToSeat[itemId].date, idToSeat[itemId].link, idToSeat[itemId].number, idToSeat[itemId].row, "Show cancelled");
+        
+    }
+    
+    
+
+    function tradeTicket(string memory titleA,
+        string memory dateA,
+        uint8 numberA,
+        uint8 rowA,
+        string memory titleB,
+        string memory dateB,
+        uint8 numberB,
+        uint8 rowB) public payable{
+        
+        // Case A: Both have tokens
+        
+        uint256 itemIdA = findSeat(titleA, dateA, numberA, rowA);
+        uint256 itemIdB = findSeat(titleB, dateB, numberB, rowB);
+        
+        address payable owner_A = idToSeat[itemIdA].owner;
+        address payable owner_B = idToSeat[itemIdB].owner;
+        
+        require(owner_A != theatre, "Seat has not been sold yet");
+        require(owner_B != theatre, "Seat has not been sold yet");
+        
+        
+        setApprovalForAll(idToSeat[itemIdA].nftContract, true);
+        setApprovalForAll(idToSeat[itemIdB].nftContract, true);
+        IERC721(address(this)).transferFrom(owner_A, owner_B, itemIdA);
+        IERC721(address(this)).transferFrom(owner_B, owner_A, itemIdB);
+        
+        idToSeat[itemIdA].owner = owner_B;
+        idToSeat[itemIdB].owner = owner_A;
+        
+        emit TicketTransfer(itemIdA, owner_A, owner_B, idToSeat[itemIdA].price, idToSeat[itemIdA].title, idToSeat[itemIdA].date, idToSeat[itemIdA].link, idToSeat[itemIdA].number, idToSeat[itemIdA].row);
+        emit TicketTransfer(itemIdB, owner_B, owner_A, idToSeat[itemIdB].price, idToSeat[itemIdB].title, idToSeat[itemIdB].date, idToSeat[itemIdB].link, idToSeat[itemIdB].number, idToSeat[itemIdB].row);
+        
+    }
+    
+    function tradeTicket(
+        string memory title,
+        string memory date,
+        uint8 number,
+        uint8 row) public payable{
+            
+        // Case B: A has token, B has coins
+        // Call only from B,send ticket price as value
+        //
+
+        uint256 itemId = findSeat(title, date, number, row);
+        address payable owner = idToSeat[itemId].owner;
+        require(owner != theatre, "Seat has not been sold yet");
+        
+        uint price = idToSeat[itemId].price;
+        require(msg.value == price, "Please send the asking price for the seat");
+        
+        IERC721(address(this)).transferFrom(owner, msg.sender, itemId);
+        owner.transfer(msg.value);
+        idToSeat[itemId].owner = payable(msg.sender);
+        
+        emit TicketTransfer(itemId, owner, payable(msg.sender), price, idToSeat[itemId].title, idToSeat[itemId].date, idToSeat[itemId].link, idToSeat[itemId].number, idToSeat[itemId].row);
+        
+    }
+        
+        
+        
     
     
   
